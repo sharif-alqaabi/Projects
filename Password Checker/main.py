4203198657afe5f6a3fea5d20fedccc30f5f110b
sharif-alqaabi <yashalqaabi@gmail.com>
2025-08-16 14:09:04 -0500

    Added user input prompt for username and password and Displayed masked password with character count

---
+++ Password Checker/main.py
@@ -0,0 +1,4 @@
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+print(f"Hey {username}, your password {"*"*len(password)} is {len(password)} characters long.")
