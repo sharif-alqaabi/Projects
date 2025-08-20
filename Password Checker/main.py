4cd2a586ff4a8b491f05cb011454a4bacba991c3
sharif-alqaabi <yashalqaabi@gmail.com>
2025-08-20 17:20:35 -0500

    Optimized the code and imporved readability

--- Password Checker/main.py
+++ Password Checker/main.py
@@ -1,4 +1,5 @@
 username = input("Enter your username: ")
 password = input("Enter your password: ")
+pass_lenght = len(password)
 
-print(f"Hey {username}, your password {"*"*len(password)} is {len(password)} characters long.")
+print(f"Hey {username}, your password {"*"*pass_lenght} is {pass_lenght} characters long.")
